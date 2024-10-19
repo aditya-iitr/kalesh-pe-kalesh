29d6609a5f437e5a837fa20212e0d3976346c844
Aditya Ranjan <ar482468@gmail.com>
2024-10-19 21:59:01 +0530

    new update

--- assets/chess.gd
+++ assets/chess.gd
@@ -1,32 +1,37 @@
 extends Sprite2D
-const row_size = 10
-const col_size = 8
+const row_size = 8
+const col_size =12
 
-const cell_width =25
+const cell_width = 12.3
+
+
+
+
+
 
 const texture_holder= preload("res://texture_holder.tscn")
 
-const archer = preload("res://assets/archers.png")
-const babydragon = preload ("res://assets/baby-dragon.png")
-const barbarian = preload("res://assets/barbarians.png")
-const bowler = preload ("res://assets/bowler.png")
-const healer = preload ("res://assets/healer.png")
-const hogrider = preload ("res://assets/hog-rider.png")
-const minion = preload("res://assets/minions.png")
-const warden = preload("res://assets/warden.png")
-const wizard = preload ("res://assets/wizard.png")
+const archer = preload("res://assets/archersn.png")
+const babydragon = preload ("res://assets/baby-dragonn.png")
+#const barbarian = preload("res://assets/barbarians.png")
+#const bowler = preload ("res://assets/bowler.png")
+#const healer = preload ("res://assets/healer.png")
+#const hogrider = preload ("res://assets/hog-rider.png")
+const minion = preload("res://assets/minionsn.png")
+#const warden = preload("res://assets/warden.png")
+#const wizard = preload ("res://assets/wizard.png")
 
 
 
-const dark_archer = preload("res://assets/dark/archer dark.jpg")
-const dark_babydragon = preload ("res://assets/dark/baby drag dark.jpg")
-const dark_barbarian = preload("res://assets/dark/barbraian dark.jpg")
-const dark_bowler = preload ("res://assets/dark/bowler dark.jpg")
-const dark_healer = preload ("res://assets/dark/healer dark.jpg")
-const dark_hogrider = preload ("res://assets/dark/hog dark.jpg")
-const dark_minion = preload("res://assets/dark/minion dark.jpg")
-const dark_warden = preload("res://assets/dark/warden dark.jpg")
-const dark_wizard = preload ("res://assets/dark/wizard dark.jpg")
+#const dark_archer = preload("res://assets/dark/archer dark.jpg")
+#const dark_babydragon = preload ("res://assets/dark/baby drag dark.jpg")
+const dark_barbarian = preload("res://assets/dark/barbraian darkn.jpg")
+#const dark_bowler = preload ("res://assets/dark/bowler dark.jpg")
+#const dark_healer = preload ("res://assets/dark/healer dark.jpg")
+#const dark_hogrider = preload ("res://assets/dark/hog dark.jpg")
+#const dark_minion = preload("res://assets/dark/minion dark.jpg")
+#const dark_warden = preload("res://assets/dark/warden dark.jpg")
+#const dark_wizard = preload ("res://assets/dark/wizard dark.jpg")
 
 
 
@@ -52,37 +57,39 @@ func _ready():
 	board.append([0,0,0,0,0,0,0,0])
 	board.append([0,0,0,0,0,0,0,0])
 	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
+	board.append([0,0,0,0,0,0,0,0])
 	board.append([-1,-2,-3,-4,-4,-3,-2,-1])
 	board.append([-5,-9,-6,-7,-8,-6,-9,-5])
 	
 	display_board()
 	
 func display_board():
-	for i in row_size:
-		for j in col_size:
+	for i in col_size:
+		for j in row_size:
 			var holder = texture_holder.instantiate()
 			pieces.add_child(holder)
-			holder.global_position= Vector2(j*cell_width+(cell_width/2),-i*cell_width-(cell_width/2))
+			holder.global_position= Vector2(j*cell_width+(cell_width/2),i*cell_width-(cell_width/2))
 			
 			match board[i][j]:
 				-1: holder.texture = dark_barbarian
-				-2: holder.texture = dark_archer
-				-3: holder.texture = dark_minion
-				-4: holder.texture = dark_bowler
-				-5: holder.texture = dark_wizard
-				-6: holder.texture = dark_babydragon
-				-7: holder.texture = dark_warden
-				-8: holder.texture = dark_healer
-				-9: holder.texture = dark_hogrider
+			#	-2: holder.texture = dark_archer
+#				-3: holder.texture = dark_minion
+#				-4: holder.texture = dark_bowler
+#				-5: holder.texture = dark_wizard
+#				-6: holder.texture = dark_babydragon
+#				-7: holder.texture = dark_warden
+#				-8: holder.texture = dark_healer
+#				-9: holder.texture = dark_hogrider
 				0: holder.texture = null
-				1: holder.texture = barbarian
+#				1: holder.texture = barbarian
 				2: holder.texture = archer
 				3: holder.texture = minion
-				4: holder.texture = bowler
-				5: holder.texture = wizard
+#				4: holder.texture = bowler
+#				5: holder.texture = wizard
 				6: holder.texture = babydragon
-				7: holder.texture = warden
-				8: holder.texture = healer
-				9: holder.texture = hogrider 
+#				7: holder.texture = warden
+#				8: holder.texture = healer
+#				9: holder.texture = hogrider 
 				
 				
